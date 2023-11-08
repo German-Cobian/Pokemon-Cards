e493a478d3050d36bafafd0901cbd416512b1cb1
German Renato Cobian <germancobian@hotmail.com>
2023-11-08 10:07:26 -0800

    Improve styling and responsiveness of the various components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import PokemonLogo from '../assets/Pokemon-logo.png';
 
 const NavBar = () => (
-  <div className="d-flex flex-row justify-content-between border-bottom">
-    <div className="mx-3">Pokemons Collection</div>
-    <div>
-      <NavLink to="/" activeClassName="active-link"><span className="me-3">Pokemon by Name</span></NavLink>
-      <NavLink to="/pokemons-type" activeClassName="active-link"><span className="me-3">Pokemon by Type</span></NavLink>
-      <NavLink to="/types-listing" activeClassName="link-style active-link"><span className="me-3">Types Listing</span></NavLink>
+  <div className="d-flex flex-row justify-content-between border-bottom bg-dark">
+    <img src={PokemonLogo} height="50" alt="Pokemon-logo" className="ms-4" />
+    <div className="mt-3 me-4">
+        <NavLink to="/" activeClassName="active-link"><span className="me-2"><small>Pokemon by Name</small></span></NavLink>
+        <NavLink to="/pokemons-type" activeClassName="active-link"><span className="me-2"><small>Pokemon by Type</small></span></NavLink>
+        <NavLink to="/types-listing" activeClassName="link-style active-link"><span className=""><small>Types Listing</small></span></NavLink>
     </div>
   </div>
 );
 
 export default NavBar;
+
+
